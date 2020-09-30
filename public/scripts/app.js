34083a85e1fcbe864346f5df7c31ec6417062d3b
SmoothDev24 <rsmithit@gmail.com>
2020-09-29 21:53:22 -0400

    Installed Babel locally using npm and refactored the JSX code so that it will be dynamic.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,71 @@
 console.log('App.js is running');
 
 // JSX - JavaScript XML
+var appTitle = {
+    title: 'Indecision App',
+    subtitle: 'This app that will make up your mind for you!'
+};
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appTitle.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        appTitle.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+var appRootTwo = document.getElementById('appTwo');
+ReactDOM.render(template, appRootTwo);
+
+var user = {
+    name: "Smooth",
+    age: 24,
+    location: 'Dubai'
+};
+var userName = 'Haol';
+var userAge = 24;
+var userLocation = 'Florida';
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRootTwo)
+ReactDOM.render(templateTwo, appRoot);
--- source/app.js
+++ source/app.js
@@ -1,7 +1,39 @@
 console.log('App.js is running')
 
 // JSX - JavaScript XML
-let template = <h1>Indecision App!</h1>
+let appTitle = {
+    title: 'Indecision App',
+    subtitle: 'This app that will make up your mind for you!'
+}
+let template = (
+    <div>
+        <h1>{appTitle.title}</h1>
+        <p>{appTitle.subtitle}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+)
+let appRootTwo = document.getElementById('appTwo')
+ReactDOM.render(template, appRootTwo)
+
+let user = {
+    name: "Smooth",
+    age: 24,
+    location: 'Dubai'
+}
+let userName = 'Haol'
+let userAge = 24
+let userLocation = 'Florida'
+let templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+)
 let appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+// ReactDOM.render(template, appRootTwo)
+ReactDOM.render(templateTwo, appRoot)
